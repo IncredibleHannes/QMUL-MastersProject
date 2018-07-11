025929d1572210ddb824f319cef92c458bc0dd8e
Johannes Hartmann <johannes.Hartmann.Calw@web.de>
2018-07-11 15:09:29 +0100

    refactored Connect Three matrix

--- testMonadic/ConnectThreeMatrix.hs
+++ testMonadic/ConnectThreeMatrix.hs
@@ -24,12 +24,12 @@ wins = checkWin 1 1
   where
     checkWin :: Int -> Int -> Board -> Player -> Bool
     checkWin 4 3 _ _ = False
-    checkWin 5 y b p = ((b ! (5, y) == p) && ((row 5 y b p) || (colum 5 y b p) || (left 5 y b p) || (right 5 y b p))) || (checkWin 1 (y + 1) b p)
-    checkWin x y b p = ((b ! (x, y) == p) && ((row x y b p) || (colum x y b p) || (left x y b p) || (right x y b p))) || (checkWin (x + 1) y b p)
-    row x y b p      = if x < 4 then b ! (x, y) == p && b ! (x+1, y) == p && b ! (x+2, y) == p else False
-    colum x y b p    = if y == 1 then b ! (x, y) == p && b ! (x, y+1) == p && b ! (x, y+2) == p else False
-    left x y b p     = if y == 1 && x < 4 then b ! (x, y) == p && b ! (x+1, y+1) == p && b ! (x+2, y+2) == p else False
-    right x y b p    = if y == 1 && x > 2 then b ! (x, y) == p && b ! (x-1, y+1) == p && b ! (x-2, y+2) == p else False
+    checkWin 5 y b p = ((b ! (5, y) == p) && (row 5 y b p || colum 5 y b p || left 5 y b p || right 5 y b p)) || checkWin 1 (y + 1) b p
+    checkWin x y b p = ((b ! (x, y) == p) && (row x y b p || colum x y b p || left x y b p || right x y b p)) || checkWin (x + 1) y b p
+    row x y b p      = x < 4 && (b ! (x, y) == p && b ! (x+1, y) == p && b ! (x+2, y) == p)
+    colum x y b p    = y == 1 && (b ! (x, y) == p && b ! (x, y+1) == p && b ! (x, y+2) == p)
+    left x y b p     = y == 1 && x < 4 && (b ! (x, y) == p && b ! (x+1, y+1) == p && b ! (x+2, y+2) == p)
+    right x y b p    = y == 1 && x > 2 && (b ! (x, y) == p && b ! (x-1, y+1) == p && b ! (x-2, y+2) == p)
 
 value :: Board -> R
 value b  | wins b X  = 1
@@ -60,7 +60,7 @@ getPossibleMoves [] = [1..5]
 getPossibleMoves xs = filter (\x -> length (L.elemIndices x xs) < 3) [1..5]
 
 epsilons :: [[Move] -> J R Move]
-epsilons = take 9 all
+epsilons = take 7 all
   where all = epsilonX : epsilonO : all
         epsilonX history = epsilonSupThree (getPossibleMoves history)
         epsilonO history = epsilonInfThree (getPossibleMoves history)
